01fb8a44ae0c8054b7ab567d5af6a544e614b774
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-03-24 23:45:33 +0530

    Update heart_disease.py

--- heart_disease.py
+++ heart_disease.py
@@ -1,6 +1,6 @@
-Write a program to construct a Bayesian network considering medical data. 
-Use this model to demonstrate the diagnosis of heart patients using standard Heart Disease Data Set. 
-You can use Java/Python ML library classes/API.
+# Write a program to construct a Bayesian network considering medical data. 
+ Use this model to demonstrate the diagnosis of heart patients using standard Heart Disease Data Set. 
+ You can use Java/Python ML library classes/API.
 
 
 
