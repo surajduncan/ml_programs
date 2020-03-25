34766ead30b587b49b15e96db02d49c6b136bd02
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-03-25 22:35:38 +0530

    Update EM_algorithm.py
    Apply EM algorithm to cluster a set of data stored in a .CSV file. Use the same data set for clustering using k-Means algorithm. Compare the results of these two algorithms and comment on the quality of clustering. You can add Java/Python ML library classes/API in the program.

--- EM_algorithm.py
+++ EM_algorithm.py
@@ -2,3 +2,44 @@
 # Use the same data set for clustering using k-Means algorithm. 
 # Compare the results of these two algorithms and comment on the quality of clustering. 
 # You can add Java/Python ML library classes/API in the program.
+
+
+
+
+
+from sklearn.cluster import KMeans
+
+#from sklearn import metrics	
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np
+iris=datasets.load_iris()
+X=pd.DataFrame(iris.data)
+X.columns=['Sepal_Length','Sepal_width','Petal_Length','Petal_Width']
+y=pd.DataFrame(iris.target)
+y.columns=['Targets']
+model=KMeans(n_clusters=3)
+model.fit(X)
+plt.figure(figsize=(14,14))
+colormap=np.array(['red','lime','black'])
+plt.subplot(2,2,1)
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[model.labels_],s=40)
+plt.title('K-Means Clustering')
+plt.xlabel('Petal Length\n')
+plt.ylabel('Petal Width\n')
+from sklearn import preprocessing
+scaler=preprocessing.StandardScaler()
+scaler.fit(X)
+xsa=scaler.transform(X)
+xs=pd.DataFrame(xsa,columns=X.columns)
+from sklearn.mixture import GaussianMixture
+gmm=GaussianMixture(n_components=3)
+gmm.fit(xs)
+gmm_y=gmm.predict(xs)
+plt.subplot(2,2,3)
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[gmm_y],s=40)
+plt.title('\nGMM Clustering')
+plt.xlabel('\nPetal Length')
+
