efa06d8c0de2d6442e5405156e688c4f9e3b1317
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-03-26 22:46:55 +0530

    Update k-nearest_neighbour.py
    Write a program to implement k-Nearest Neighbor algorithm to classify the iris data set. Print both correct and wrong predictions. Java/Python ML library classes can be used for this problem.

--- k-nearest_neighbour.py
+++ k-nearest_neighbour.py
@@ -1,3 +1,3 @@
-Write a program to implement k-Nearest Neighbor algorithm to classify the iris data set.
-Print both correct and wrong predictions. 
-Java/Python ML library classes can be used for this problem.
+# Write a program to implement k-Nearest Neighbor algorithm to classify the iris data set.
+# Print both correct and wrong predictions. 
+# Java/Python ML library classes can be used for this problem.
